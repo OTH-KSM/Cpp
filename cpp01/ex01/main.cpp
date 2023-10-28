80e0af33ee61736884f5348922191ccfeb370e43
OTH-KSM <ddosddos010101@gmail.com>
2023-10-28 20:17:02 +0100

    tocheck is 8 the index to move from object to another

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -4,9 +4,16 @@ using namespace std;
 int main ( void )
 {
     Zombie *tmp = zombieHorde(8, "Ahmed");
+    Zombie *head = tmp;
 
     for (int i = 0; i < 8; i++) {
+        cout << sizeof(tmp + i) << endl;
         cout << tmp + i << endl;
         (tmp + i)->announce();
+        // system("leaks Zombie");
     }
+    // for (int i = 7; i > 0; i++) {
+    //     cout << (*head) + i << "Freed Seccesfully" << endl;
+    //     delete (head + i);
+    // }
 }
